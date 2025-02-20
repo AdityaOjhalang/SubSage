a956a0e8d769c3cbbf467ad02675d27f5a86d402
Aditya Ojha <aditya.ojha2820@gmail.com>
2025-02-19 20:22:08 -0500

    Added mongodb and mongoose connectivity

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import {PORT} from './config/env.js';
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscriptions.routes.js';
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 app.use(express.json());
@@ -15,8 +16,9 @@ app.get('/', (req , res) => {
   res.send('Welcome to Sub tracker API');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
+  await connectToDatabase()
 });
 
 export default app;
